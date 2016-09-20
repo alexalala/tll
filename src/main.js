fd7b35b9af024fd495a26348479345a76eb484e5
Alexandra Moore <alexmoore94@hotmail.co.uk>
2016-09-20 19:23:51 +0100

    added object of preloaded images

--- src/main.js
+++ src/main.js
@@ -5,25 +5,25 @@ $(window).load(function() {
 
 var images = [];
 var imagesLeft = [
-    {url:'img/pics/cameo.jpg', description:'The Cameo Cinema'}, 
-    {url:'img/pics/carlisleStreetWavertree.jpg', description:'Carlisle Street, Wavertree'}, 
-    {url:'img/pics/fallingStar.jpg', description:'Falling Star'}, 
-    {url:'img/pics/gasWorksSpofforthRoad.jpg', description:'Spofforth Road Gas Works'}, 
-    {url:'img/pics/grandCinema.jpg', description:'Grand Cinema'}, 
-    {url:'img/pics/hairdresser.jpg', description:'Hairdresser'}, 
-    {url:'img/pics/highStreetWavertree.jpg', description:'Wavertree High Street'}, 
-    {url:'img/pics/holyTrinity.jpg', description:'Holy Trinity'}, 
-    {url:'img/pics/magnetCinema.jpg', description:'Magnet Cinema'}, 
-    {url:'img/pics/pictonClockTower.jpg', description:'Picton Clock Tower'}, 
-    {url:'img/pics/pyeStreet.jpg', description:'Pye Street'}, 
-    {url:'img/pics/railwaySidingsWavertree.jpg', description:'Railway Sidings, Wavertree'}, 
-    {url:'img/pics/theLoversFirLane.jpg', description:'The Lovers, Fir Lane'}, 
-    {url:'img/pics/theStreetSinger.jpg', description:'The Street Singer'}, 
-    {url:'img/pics/theWellington.jpg', description:'The Wellington'}, 
-    {url:'img/pics/victorianDrinkingFountain.jpg', description:'Victorian Drinking Fountain'}, 
-    {url:'img/pics/victorianLetterbox.jpg', description:'Victorian Letterbox'}, 
-    {url:'img/pics/wavertreeTrainStation.jpg', description:'Wavertree Train Station'}, 
-    {url:'img/pics/wellingtonAvenueWavertree.jpg', description:'Wellington Avenue, Wavertree'}
+    {url:'img/pics/cameo.jpg', name:'The Cameo Cinema'}, 
+    {url:'img/pics/carlisleStreetWavertree.jpg', name:'Carlisle Street, Wavertree'}, 
+    {url:'img/pics/fallingStar.jpg', name:'Falling Star'}, 
+    {url:'img/pics/gasWorksSpofforthRoad.jpg', name:'Spofforth Road Gas Works'}, 
+    {url:'img/pics/grandCinema.jpg', name:'Grand Cinema'}, 
+    {url:'img/pics/hairdresser.jpg', name:'Hairdresser'}, 
+    {url:'img/pics/highStreetWavertree.jpg', name:'Wavertree High Street'}, 
+    {url:'img/pics/holyTrinity.jpg', name:'Holy Trinity'}, 
+    {url:'img/pics/magnetCinema.jpg', name:'Magnet Cinema'}, 
+    {url:'img/pics/pictonClockTower.jpg', name:'Picton Clock Tower'}, 
+    {url:'img/pics/pyeStreet.jpg', name:'Pye Street'}, 
+    {url:'img/pics/railwaySidingsWavertree.jpg', name:'Railway Sidings, Wavertree'}, 
+    {url:'img/pics/theLoversFirLane.jpg', name:'The Lovers, Fir Lane'}, 
+    {url:'img/pics/theStreetSinger.jpg', name:'The Street Singer'}, 
+    {url:'img/pics/theWellington.jpg', name:'The Wellington'}, 
+    {url:'img/pics/victorianDrinkingFountain.jpg', name:'Victorian Drinking Fountain'}, 
+    {url:'img/pics/victorianLetterbox.jpg', name:'Victorian Letterbox'}, 
+    {url:'img/pics/wavertreeTrainStation.jpg', name:'Wavertree Train Station'}, 
+    {url:'img/pics/wellingtonAvenueWavertree.jpg', name:'Wellington Avenue, Wavertree'}
 ];
 var imagesPerScreen = 5;
 var imagesTaken;
@@ -31,6 +31,7 @@ var descriptionIndex = 0;
 var offset = 50;
 var alreadyLoading;
 
+//gallery code
 if ($("body").data("title") === "gallery") {
     $(window).scroll(function() {
         if (($(window, ".gallery").scrollTop() + $(window).height() + offset >= $(document).height()) && alreadyLoading !== true)
@@ -43,11 +44,11 @@ if ($("body").data("title") === "gallery") {
 
             // loop through the images you have retrieved and make HTML using jQuery and store into a variable
             $(imagesTaken).each(function(index, value){
-                var $desc = value.description;
+                var $name = value.name;
                 var $image = value.url;
                 var $sliceOffUrl = $image.slice(9);
                 var $simpleDesc = $sliceOffUrl.split('.')[0];
-                var $imgStyle = $('<div class="col-xs-12 col-md-6 galiria"><figure class="captionjs animated"><figcaption itemprop="name" style="margin-bottom:0px; bottom:-58px;">' + $desc + '</figcaption><img class="img1" src="' + $image + '" alt="' + $simpleDesc + '"></figure></div>');
+                var $imgStyle = $('<div class="col-xs-12 col-md-6 galiria"><figure class="captionjs animated"><figcaption itemprop="name" style="margin-bottom:0px; bottom:-58px;">' + $name + '</figcaption><img class="img1" src="' + $image + '" alt="' + $simpleDesc + '"></figure></div>');
                 images.push(value);
                 imagesLeft.splice(value, 1);
                 
@@ -93,8 +94,8 @@ if ($("body").data("title") === "imageView") {
     })();
 
     //add description to imageViewer
-    imageLongDesc = imageObject.description;
-    $(".titleHolder").append('<h2 class="imageTitle">'+imageLongDesc+'</h2>');
+    imageName = imageObject.name;
+    $(".titleHolder").append('<h2 class="imageTitle">'+imageName+'</h2>');
 
     //add image to imageViewer
     imageUrl = imageObject.url;
