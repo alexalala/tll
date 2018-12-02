aa77138e0dee1d91bed002d38a52ab192b4c8a19
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-12-02 22:03:42 +0000

    pages de-handlebar'd

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 $(window).load(function() {
-		$('img.caption').captionjs();		
+		$('img.caption').captionjs();
 		var galiria = new Galiria();
 });
 
@@ -12,27 +12,27 @@ var preloadedImages = [
     {url:'img/pics/shankly.jpg', name: 'Bill Shankly', desc:'Bill Shankly was a Scottish footballing legend best known for being the manager of Liverpool. He took them over when they were in the second division and not only got them promoted to the first division, but also won 3 division championships, 2 FA cups and one UEFA cup with them - molding them into the world class team they are today.'},
     {url:'img/pics/cambridge-street-mission.jpg', name: 'The Cambridge Street Mission, Wavertree', desc:'A portrayal of the Cambridge Street Mission, on Cambridge Street, Wavertree. It is now student accomodation.'},
     {url:'img/pics/cameo.jpg', name:'The Cameo Cinema', desc:'The Cameo Cinema in Wavertree became infamous for a double murder of the cinema manager and his assistant for the day\'s box office takings in 1949. George Kelly was arrested, found guilty and subsequently (and controversially) was hung for the crime.'},
-    {url:'img/pics/carlisle-street-wavertree.jpg', name:'Carlisle Street, Wavertree', desc:'This image shows Carlisle Street in the depths of winter, with the Gasometers in the background. All of the houses in this image have since been demolished in an attempt to modernise the streets.'}, 
-    {url:'img/pics/falling-star.jpg', name:'Falling Star', desc:'A shooting star above the Wavertree skyline, showing St. Bridgets Church in shadow.'} 
+    {url:'img/pics/carlisle-street-wavertree.jpg', name:'Carlisle Street, Wavertree', desc:'This image shows Carlisle Street in the depths of winter, with the Gasometers in the background. All of the houses in this image have since been demolished in an attempt to modernise the streets.'},
+    {url:'img/pics/falling-star.jpg', name:'Falling Star', desc:'A shooting star above the Wavertree skyline, showing St. Bridgets Church in shadow.'}
 ];
 
 var images = [];
-var imagesLeft = [ 
-    {url:'img/pics/gas-works-spofforth-road.jpg', name:'The Spofforth Road Gas Works', desc:'The background of this image shows the (now derelict) gasometers in Wavertree, built in the 1840\'s.'}, 
-    {url:'img/pics/grand-cinema.jpg', name:'The Grand Cinema', desc:'This picture depicts the Grand Cinema on Smithdown Road in all its glory. It no longer exists.'}, 
-    {url:'img/pics/hairdresser.jpg', name:'The Hairdresser', desc:'A Wavertree Hairdressers, belonging to Bob Cochran (Bunty). At the time he only charged 20p for a haircut'}, 
-    {url:'img/pics/high-street-wavertree.jpg', name:'Wavertree High Street', desc:'This painting shows the High Street of Wavertree covered in a blanket of snow. In the background you can see the Picton Clock Tower. Reference to Wavertree can be found in the Doomsday Book, where it was referred to as "Wartree", "Vauretea" and "Wavre".'}, 
-    {url:'img/pics/holy-trinity.jpg', name:'The Holy Trinity Church', desc:'Built in 1794, the Holy Trinity Church is an active Anglican Parish Church in the deanery of Toxteth and Wavertree. More information on the church can be found on <a href="http://www.holytrinitywavertree.org.uk">this website</a>. This picture shows the church at Christmas time.'}, 
-    {url:'img/pics/magnet-cinema.jpg', name:'The Magnet Cinema', desc:'The Magnet Cinema was opened in 1914 on Picton Road and only had one screen, yet could seat up to 1038 people. The building has now lost its roof and has ironically been replaced with a roofing company.'}, 
-    {url:'img/pics/picton-clock-tower.jpg', name:'The Picton Clock Tower', desc:'Created in 1884 by James Picton as a memorial to his wife, this tower has a clock face on each of its 4 sides.'}, 
-    {url:'img/pics/pye-street.jpg', name:'Pye Street', desc:'Pye Street is a small, narrow lane that is round the back of the old ESSO petrol station. It still has some remarkable features that have remained, such as the victorian streetlights depicted in the painting.'}, 
-    {url:'img/pics/railway-sidings-wavertree.jpg', name:'Railway Sidings, Wavertree', desc:'This painting is a night scene, illustrating railway workers loading the trains up with coal.. well before the days of health and safety! These are the Down Hill sidings on Edge Hill.'}, 
-    {url:'img/pics/the-lovers-fir-lane.jpg', name:'The Lovers, Fir Lane', desc:'A black and white painting, showing a lady and gentleman strolling through Fir Lane in the winter. In the background you can see the Blue Coat School, St. John\'s beacon (Radio City Tower) and both Liverpool Cathedrals.'}, 
-    {url:'img/pics/the-street-singer.jpg', name:'The Street Singer', desc:'A street singer that travelled around the district quite regularly, earning his living.'}, 
-    {url:'img/pics/the-wellington.jpg', name:'The Wellington', desc:'The Wellington Public House on Picton Road, with its vibrant red exterior. It is locally known as \'The Wellie\''}, 
-    {url:'img/pics/victorian-drinking-fountain.jpg', name:'Victorian Drinking Fountain', desc:'After epidemics like Cholera, public drinking fountains became popular in the Victorian Age as they were a rare clean water supply for the masses. In northern cities like Liverpool and Hull, they were supplied by civic authorities. This particular one stands on Picton Road and remains as decoration.'}, 
-    {url:'img/pics/victorian-letterbox.jpg', name:'Victorian Letterbox', desc:'This painting shows a little bit of history among today\'s 21st Century Life. The old Victorian Letterbox shown in this image has remained functional until relatively recently, when it was destroyed by vandals lighting a firework inside of it.'}, 
-    {url:'img/pics/wavertree-train-station.jpg', name:'Wavertree Train Station', desc:'Wavertree Station was opened in 1891 and was on the main southern line from Liverpool Lime Street. As can be seen in the picture, the station was accessed from between the bridges that supported the lines. The station closed in 1958 and nothing now remains at that part of the track.'}, 
+var imagesLeft = [
+    {url:'img/pics/gas-works-spofforth-road.jpg', name:'The Spofforth Road Gas Works', desc:'The background of this image shows the (now derelict) gasometers in Wavertree, built in the 1840\'s.'},
+    {url:'img/pics/grand-cinema.jpg', name:'The Grand Cinema', desc:'This picture depicts the Grand Cinema on Smithdown Road in all its glory. It no longer exists.'},
+    {url:'img/pics/hairdresser.jpg', name:'The Hairdresser', desc:'A Wavertree Hairdressers, belonging to Bob Cochran (Bunty). At the time he only charged 20p for a haircut'},
+    {url:'img/pics/high-street-wavertree.jpg', name:'Wavertree High Street', desc:'This painting shows the High Street of Wavertree covered in a blanket of snow. In the background you can see the Picton Clock Tower. Reference to Wavertree can be found in the Doomsday Book, where it was referred to as "Wartree", "Vauretea" and "Wavre".'},
+    {url:'img/pics/holy-trinity.jpg', name:'The Holy Trinity Church', desc:'Built in 1794, the Holy Trinity Church is an active Anglican Parish Church in the deanery of Toxteth and Wavertree. More information on the church can be found on <a href="http://www.holytrinitywavertree.org.uk">this website</a>. This picture shows the church at Christmas time.'},
+    {url:'img/pics/magnet-cinema.jpg', name:'The Magnet Cinema', desc:'The Magnet Cinema was opened in 1914 on Picton Road and only had one screen, yet could seat up to 1038 people. The building has now lost its roof and has ironically been replaced with a roofing company.'},
+    {url:'img/pics/picton-clock-tower.jpg', name:'The Picton Clock Tower', desc:'Created in 1884 by James Picton as a memorial to his wife, this tower has a clock face on each of its 4 sides.'},
+    {url:'img/pics/pye-street.jpg', name:'Pye Street', desc:'Pye Street is a small, narrow lane that is round the back of the old ESSO petrol station. It still has some remarkable features that have remained, such as the victorian streetlights depicted in the painting.'},
+    {url:'img/pics/railway-sidings-wavertree.jpg', name:'Railway Sidings, Wavertree', desc:'This painting is a night scene, illustrating railway workers loading the trains up with coal.. well before the days of health and safety! These are the Down Hill sidings on Edge Hill.'},
+    {url:'img/pics/the-lovers-fir-lane.jpg', name:'The Lovers, Fir Lane', desc:'A black and white painting, showing a lady and gentleman strolling through Fir Lane in the winter. In the background you can see the Blue Coat School, St. John\'s beacon (Radio City Tower) and both Liverpool Cathedrals.'},
+    {url:'img/pics/the-street-singer.jpg', name:'The Street Singer', desc:'A street singer that travelled around the district quite regularly, earning his living.'},
+    {url:'img/pics/the-wellington.jpg', name:'The Wellington', desc:'The Wellington Public House on Picton Road, with its vibrant red exterior. It is locally known as \'The Wellie\''},
+    {url:'img/pics/victorian-drinking-fountain.jpg', name:'Victorian Drinking Fountain', desc:'After epidemics like Cholera, public drinking fountains became popular in the Victorian Age as they were a rare clean water supply for the masses. In northern cities like Liverpool and Hull, they were supplied by civic authorities. This particular one stands on Picton Road and remains as decoration.'},
+    {url:'img/pics/victorian-letterbox.jpg', name:'Victorian Letterbox', desc:'This painting shows a little bit of history among today\'s 21st Century Life. The old Victorian Letterbox shown in this image has remained functional until relatively recently, when it was destroyed by vandals lighting a firework inside of it.'},
+    {url:'img/pics/wavertree-train-station.jpg', name:'Wavertree Train Station', desc:'Wavertree Station was opened in 1891 and was on the main southern line from Liverpool Lime Street. As can be seen in the picture, the station was accessed from between the bridges that supported the lines. The station closed in 1958 and nothing now remains at that part of the track.'},
     {url:'img/pics/wellington-avenue-wavertree.jpg', name:'Wellington Avenue, Wavertree', desc:'This painting portrays Wellington Avenue in Wavertree, which has now since been modernised.'}
 ];
 var imagesPerScreen = 6;
@@ -41,12 +41,12 @@ var offset = 50;
 var alreadyLoading;
 
 //gallery code
-if (window.location.pathname == "/") {
+if (window.location.pathname == "index.html") {
     $(window).scroll(function(alreadyLoading, imagesTaken) {
-        if (($(window, ".gallery").scrollTop() + $(window).height() + offset >= $(document).height()) && alreadyLoading !== true) { 
-            
+        if (($(window, ".gallery").scrollTop() + $(window).height() + offset >= $(document).height()) && alreadyLoading !== true) {
+
             alreadyLoading = true;
-           
+
             // take imagesPerScreen number of images out of imagesLeft and into its own variable
             imagesTaken = imagesLeft.slice(0, imagesPerScreen);
 
@@ -59,17 +59,17 @@ if (window.location.pathname == "/") {
                 var $imgStyle = $('<div class="col-xs-6 col-md-6 galiria"><figure class="captionjs animated"><figcaption style="margin-bottom:0px; bottom:-58px;">' + $name + '</figcaption><img class="img1" src="' + $image + '" alt="' + $name + '" id="' + $simpleDesc + '"></figure></div>');
                 images.push(value);
                 imagesLeft.splice(value, 1);
-                
-                  
-                // append the html content in the variable to the end of the existing images in the DOM             
-                $(".contentRow").append($imgStyle); 
+
+
+                // append the html content in the variable to the end of the existing images in the DOM
+                $(".contentRow").append($imgStyle);
             });
         }
         //on click load imageviewer and img description into hash
         $("img").ready(function(){
             $(".img1").click(function(){
                 var src = $(this).attr("id");
-                window.location.href = '/imageView#' + src;
+                window.location.href = 'imageView.html#' + src;
             });
         });
 
@@ -79,7 +79,7 @@ if (window.location.pathname == "/") {
 
 
 //image view page code
-if (window.location.pathname.indexOf("/imageView") === 0) {
+if (window.location.pathname.indexOf("imageView.html") === 0) {
 
     //get short desc from url
     var imgShortDesc = location.hash.substr(1);
@@ -93,7 +93,7 @@ if (window.location.pathname.indexOf("/imageView") === 0) {
         function getDescription(obj){
             return obj.url === findByUrl;
         }
-        
+
         imageObject = imagesLeft.find(getDescription) || preloadedImages.find(getDescription);
     })();
 
@@ -116,7 +116,7 @@ if (window.location.pathname.indexOf("/imageView") === 0) {
 
     //function to allow a next button to scroll through pictures
 
-    //if not the last picture in preloaded images 
+    //if not the last picture in preloaded images
 
     //else images left array
 
